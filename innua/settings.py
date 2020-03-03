06550771f632ee789d9c6823c12e7c767e6623c6
extraordinaire01 <peter@chenchatech.com>
2020-03-03 13:54:07 +0300

    Can now pick date but not time.

--- innua/settings.py
+++ innua/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'materials',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
--- materials/forms.py
+++ materials/forms.py
@@ -3,10 +3,16 @@ from django.forms import ModelForm
 from django import forms
 from django.contrib.admin import widgets
 from django.contrib.admin.widgets import AdminDateWidget
+from .widgets import BootstrapDateTimePickerInput
+
 
 class MaterialDeliveryForm(ModelForm):
 
-    date_received = forms.DateTimeField(widget=AdminDateWidget())
+    date_received = forms.DateTimeField(
+        widget=forms.DateTimeInput(
+        attrs={'type': 'date'}
+    )
+)
     class Meta:
         model = Materials
         fields = ['house_id', 'item_name', 'date_received', 'quantity', 'item_unit']
