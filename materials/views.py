2e2a5fe921d334475413b31b691cacfb85c62920
extraordinaire01 <peter@chenchatech.com>
2020-03-05 15:30:46 +0300

    clear the form on save.

--- materials/views.py
+++ materials/views.py
@@ -3,12 +3,14 @@ from .forms import MaterialDeliveryForm
 from django.utils import timezone
 from django.views.generic.list import ListView
 from .models import Materials
+from django.http import HttpResponse, HttpResponseRedirect
 
 def material_delivery_create_view(request):
     form = MaterialDeliveryForm(request.POST or None)
 
     if form.is_valid():
         form.save()
+        return HttpResponseRedirect('')
 
     context = {
         'form':form
